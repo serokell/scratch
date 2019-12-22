4ec70b2a36adf5cc232e02c6598a824797b9fd23
Alexander Bantyev <balsoft75@gmail.com>
2019-12-22 21:11:50 +0300

    fixup! Add a script to release static binary (from CI)
    Add comments

--- scripts/release-binary.sh
+++ scripts/release-binary.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p gitAndTools.hub git -i bash
+
+# Project name, inferred from repository name
 project=$(basename $(pwd))
 
+# The directory in which tarball will be created
 TEMPDIR=`mktemp -d`
 
+# Build release/default.nix
 nix build -f release -o $TEMPDIR/$project
 
+# Create a tarball with the result
 tar --owner=serokell:1000 --mode='u+rwX' -czhf $TEMPDIR/release.tar.gz -C $TEMPDIR $project
 
+# Delete release and tag
 hub release delete auto-release
-
 git tag -d auto-release
 
+# Create release
 hub release create -a $TEMPDIR/release.tar.gz -m "Automatic build on $(date -I)" --prerelease auto-release
