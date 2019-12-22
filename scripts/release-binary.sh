fdd3ebddba84b26116c3a807168e1f12ecaaf442
Alexander Bantyev <balsoft75@gmail.com>
2019-12-22 21:08:27 +0300

    fixup! Add a script to release static binary (from CI)

--- scripts/release-binary.sh
+++ scripts/release-binary.sh
@@ -4,12 +4,12 @@ project=$(basename $(pwd))
 
 TEMPDIR=`mktemp -d`
 
-REV=`git rev-parse HEAD`
-
 nix build -f release -o $TEMPDIR/$project
 
 tar --owner=serokell:1000 --mode='u+rwX' -czhf $TEMPDIR/release.tar.gz -C $TEMPDIR $project
 
 hub release delete auto-release
+
+git tag -d auto-release
 
 hub release create -a $TEMPDIR/release.tar.gz -m "Automatic build on $(date -I)" --prerelease auto-release
