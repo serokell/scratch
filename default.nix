5018330dfa816f422bc14c0045634561fe8fd74c
Yorick van Pelt <yorick@yorickvanpelt.nl>
2020-02-19 14:03:00 +0100

    add default.nix for limited reproducability

---
+++ default.nix
@@ -0,0 +1,55 @@
+with import <nixpkgs> {};
+let
+  installWrapped = {
+    src,
+      name ? lib.head (lib.splitString "." (builtins.baseNameOf src)),
+      buildInputs
+  }: runCommand "skl-scratch-${name}" {
+    inherit buildInputs;
+    isPy = lib.hasSuffix ".py" (toString src);
+  } ''
+  set +x
+  mkdir -p $out/bin
+  if [ "$isPy" -eq 1 ]; then
+     cat <<EOF - ${src} > $out/bin/${name}
+  #!/usr/bin/env python
+  import os
+  os.environ['PATH'] += "${lib.makeBinPath buildInputs}"
+  EOF
+  else
+     cat <<EOF - ${src} > $out/bin/${name}
+  #!/usr/bin/env bash
+  PATH = $PATH:"${lib.makeBinPath buildInputs}"
+  EOF
+  fi
+  chmod +x $out/bin/${name}
+  patchShebangs $out/bin
+'';
+  scripts = {
+    # restore-jupiter-state = installWrapped {
+    #   src = ./scripts/restore-jupiter-state.sh;
+    #   buildInputs = [ cli53 ];
+    # };
+    update-niv = installWrapped {
+      src = ./scripts/update-niv.sh;
+      buildInputs = [ jq gitAndTools.hub niv ];
+    };
+    release-binary = installWrapped {
+      src = ./scripts/release-binary.sh;
+      buildInputs = [ gitAndTools.hub git ];
+    };
+    update-stack-shas = installWrapped {
+      src = ./scripts/update-stack-shas.py;
+      buildInputs = [
+        (python3.withPackages (p: with p; [ pyyaml ]))
+        nix-prefetch-git
+      ];
+    };
+    upload-daemon = haskellPackages.callPackage ./scripts/upload-daemon {};
+  };
+in
+symlinkJoin rec {
+  name = "serokell-scratch";
+  passthru = { inherit scripts; };
+  paths = lib.collect lib.isDerivation passthru;
+}
